ec41d818ad8b8ff798ba516e622f104d722d6f83
mukil-bluerider <mrmukil1991@gmail.com>
2023-11-25 00:17:38 +0530

    Update

---
+++ ffmpeg recorder.bat
@@ -0,0 +1,1 @@
+ffmpeg -f gdigrab -video_size 1920x1080 -framerate 60 -thread_queue_size 1024 -i desktop -f dshow -i audio="Stereo Mix (Realtek High Definition Audio)" -c:v h264_nvenc -profile:v high -b:v 6M -minrate 6M -maxrate 6M -bufsize 12M -bf 2 -preset slow -c:a aac -y output.mp4
--- test.bat
+++
@@ -1,1 +0,0 @@
-ffmpeg -f gdigrab -video_size 1920x1080 -framerate 30 -thread_queue_size 1024 -i desktop -f dshow -i audio="Stereo Mix (Realtek High Definition Audio)" -c:v h264_nvenc -profile:v high -b:v 6M -minrate 6M -maxrate 6M -bufsize 12M -bf 2 -preset fast -c:a aac -y output.mp4
